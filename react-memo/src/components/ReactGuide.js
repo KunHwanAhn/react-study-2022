9ddcd8d0aa2253563499ad53aa0b1c50cd093755
Conan Kunhwan Ahn <kunhwon@gmail.com>
2022-02-02 23:57:43 +0900

    ReactMemo - 기본 컴포넌트 제거 후 강좌용 기본 컴포넌트 추가

---
+++ react-memo/src/components/ReactGuide.js
@@ -0,0 +1,14 @@
+function ReactGuide() {
+  return (
+    <div>
+      <h1>강좌 리스트</h1>
+      <ul>
+        <li>리액트 소개</li>
+        <li>리액트 개발환경구축</li>
+        <li>리액트 HelloWorld</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ReactGuide;
--- react-memo/src/index.js
+++ react-memo/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ReactGuide from './components/ReactGuide';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReactGuide/>
   </React.StrictMode>,
   document.getElementById('root')
 );
