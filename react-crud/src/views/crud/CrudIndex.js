f3562f1e6c92bdcf4c339f7e2ae03238138c46de
Conan Kunhwan Ahn <kunhwon@gmail.com>
2022-02-13 17:53:48 +0900

    User 목록 불러오기

--- react-crud/src/views/crud/CrudIndex.js
+++ react-crud/src/views/crud/CrudIndex.js
@@ -8,13 +8,64 @@ export default class CrudIndex extends Component {
       users: [],
       loading: false,
     };
+
+    this.getUsers = this.getUsers.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.getUsers();
+  }
+
+  async getUsers() {
+    this.setState({ loading: true });
+
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      const users = await res.json();
+
+      this.setState({ users });
+    } catch (error) {
+      console.error(error);
+    }
+
+    this.setState({ loading: false });
+  }
+
+  renderUserTable(users) {
+    return (
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Username</td>
+            <td>Email</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user) => {
+            return (
+              <tr key={`user-${user.id}`}>
+                <td>{ user.id }</td>
+                <td>{ user.name }</td>
+                <td>{ user.username }</td>
+                <td>{ user.email }</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
   }
 
   render() {
+    const contents = this.state.loading ? <p><em>Loading...</em></p> : this.renderUserTable(this.state.users);
+
     return (<>
       <div>
         <h1>CRUD</h1>
         <h3>리액트를 사용한 CRUD 예제 프로젝트입니다.</h3>
+        {contents}
       </div>
     </>);
   }
